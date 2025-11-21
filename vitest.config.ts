de4077df4628f60044d166f0b5d177de678d88da
zhong666 <aa900031@gmail.com>
2025-11-21 10:44:17 +0800

    chore(ci): control test report & coverage for cli args

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,15 +2,10 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
 	test: {
-		dir: './src',
 		environment: 'happy-dom',
 		coverage: {
-			enabled: true,
 			provider: 'istanbul',
 		},
-		reporters: [
-			'junit',
-		],
 		outputFile: {
 			junit: './test-reports/junit.xml',
 		},
