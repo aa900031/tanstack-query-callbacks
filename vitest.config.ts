75f186347d69555a56aae2bd0204a63442380391
zhong666 <aa900031@gmail.com>
2026-02-21 12:37:38 +0800

    chore: fix coverage for svelte

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,22 @@
+import { env } from 'node:process'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { svelteTesting } from '@testing-library/svelte/vite'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
 	plugins: [
-		svelte(),
+		svelte({
+			hot: !env.VITEST,
+		}),
 		svelteTesting(),
 	],
 	test: {
 		environment: 'happy-dom',
 		coverage: {
-			provider: 'istanbul',
+			provider: 'v8',
+			include: [
+				'src/**/*',
+			],
 		},
 		outputFile: {
 			junit: './test-reports/junit.xml',
